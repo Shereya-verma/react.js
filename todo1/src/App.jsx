349482d1e437e5fe202e10bea553f1be1f5b4f6b
Shereya Verma <sv0087218@gmail.com>
2026-07-08 22:49:35 +0530

    Remove todo functionality and simplify App component to display a static message

--- todo1/src/App.jsx
+++ todo1/src/App.jsx
@@ -4,69 +4,10 @@ import { useState } from "react";
 import "./App.css";
 
 export default function App() {
-  const [todos, setTodos] = useState([
-    {
-      title: "Go to gym",
-      description: "Hit the gym regularly",
-      done: true,
-    },
-  ]);
-
-  function addTodo() {
-    let newArray = [];
-
-    for (let i = 0; i < todos.length; i++) {
-      newArray.push(todos[i]);
-    }
-
-    newArray.push({
-      title: document.getElementById("title").value,
-      description: document.getElementById("description").value,
-      done: true,
-    });
-
-    setTodos(newArray);
+  return (
+    <div>
+      to do list
+    </div>
+  )
+ 
   }
-
-  return (
-    <div>
-      <input
-        id="title"
-        type="text"
-        placeholder="Title"
-      ></input>
-
-      <input
-        id="description"
-        type="text"
-        placeholder="Description"
-      ></input>
-
-      <br />
-
-      <button onClick={addTodo}>Add todo</button>
-
-      <br />
-
-      {todos.map((todo) => {
-        return (
-          <Todo
-            title={todo.title}
-            description={todo.description}
-            done={todo.done}
-          />
-        );
-      })}
-    </div>
-  );
-}
-
-function Todo(props) {
-  return (
-    <div>
-      <h1>{props.title}</h1>
-      <h2>{props.description}</h2>
-      <h3>{props.done ? "Done" : "Not Done"}</h3>
-    </div>
-  );
-}
